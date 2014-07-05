f7d7ad62777a876329ac9567c185b5e610897fb9
rkoeninger <rkoeninger@att.net>
2014-07-05 13:28:06 -0400

    Added Lens

---
+++ ZedSharp/Lens.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ZedSharp
+{
+    public static class Lens
+    {
+        public static Lens<A, B> Of<A, B>(Func<A, B> getter, Func<A, B, A> setter)
+        {
+            return new Lens<A, B>(getter, setter);
+        }
+    }
+
+    public struct Lens<A, B>
+    {
+        internal Lens(Func<A, B> getter, Func<A, B, A> setter) : this()
+        {
+            Get = getter;
+            Set = setter;
+        }
+
+        public Func<A, B> Get { get; private set; }
+        public Func<A, B, A> Set { get; private set; }
+
+        public Lens<A, C> Compose<C>(Lens<B, C> other)
+        {
+            var me = this;
+            return new Lens<A, C>(
+                a => other.Get(me.Get(a)),
+                (a, c) => me.Set(a, other.Set(me.Get(a), c)));
+        }
+    }
+}
