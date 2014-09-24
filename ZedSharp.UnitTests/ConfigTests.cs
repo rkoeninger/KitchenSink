8724411fa7efd2ce2f1538f5e20a54d03b1ae08e
rkoeninger <rkoeninger@att.net>
2014-09-23 23:10:19 -0400

    Added CodeLoader for csconfig files

---
+++ ZedSharp.UnitTests/ConfigTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZedSharp.UnitTests
+{
+    public interface IConfig
+    {
+        String BrandName { get; }
+
+        String FormatName(String name);
+    }
+
+    public static class App
+    {
+        private static readonly String path = "Test.csconfig";
+        private static readonly CodeLoader<IConfig> loader = new CodeLoader<IConfig>(path);
+        public static void LoadConfig() { loader.Load(); }
+        public static IConfig Config { get { return loader.Value; } }
+    }
+
+    [TestClass]
+    public class ConfigTests
+    {
+        [TestMethod]
+        public void TestConfigLoading()
+        {
+            App.LoadConfig();
+            Assert.AreEqual("Brand Name", App.Config.BrandName);
+            Assert.AreEqual("BOB", App.Config.FormatName("bob"));
+        }
+    }
+}
