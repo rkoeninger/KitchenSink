dba91c58fa4f6a1b2b753ba50a9a1cd664b5a61e
rkoeninger <rkoeninger@att.net>
2014-11-22 22:32:38 -0500

    Added KeyValuePair.Of

---
+++ ZedSharp/KeyValuePair.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZedSharp
+{
+    public static class KeyValuePair
+    {
+        public static KeyValuePair<A, B> Of<A, B>(A key, B value)
+        {
+            return new KeyValuePair<A, B>(key, value);
+        }
+    }
+}
