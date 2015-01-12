6a9938e812a799699e8f3604393cbe258f198875
rkoeninger <rkoeninger@att.net>
2015-01-12 15:17:32 -0500

    Moved Collections-related helpers into Collections.cs

--- ZedSharp/Collections.cs
+++ ZedSharp/Collections.cs
@@ -113,4 +113,131 @@ namespace ZedSharp
             return values;
         }
     }
+
+    public static class KeyValuePair
+    {
+        public static KeyValuePair<A, B> Of<A, B>(A key, B value)
+        {
+            return new KeyValuePair<A, B>(key, value);
+        }
+    }
+
+    public static class List
+    {
+        public static List<A> Of<A>(params A[] vals)
+        {
+            return new List<A>(vals);
+        }
+
+        public static List<A> Concat<A>(this List<A> list, params A[] vals)
+        {
+            var result = new List<A>(list);
+            list.AddRange(vals);
+            return result;
+        }
+
+        public static bool IsEmpty<A>(this IList<A> list)
+        {
+            return list.Count == 0;
+        }
+
+        public static bool IsNotEmpty<A>(this IList<A> list)
+        {
+            return list.Count > 0;
+        }
+
+        public static IEnumerable<int> Indicies<A>(this IList<A> list)
+        {
+            return Enumerable.Range(0, list.Count);
+        }
+
+        public static IList<A> RemoveAll<A>(this IList<A> list, IEnumerable<A> seq)
+        {
+            foreach (var item in seq)
+                list.Remove(item);
+
+            return list;
+        }
+    }
+
+    public static class AnArray
+    {
+        public static A[] Of<A>(params A[] vals)
+        {
+            return vals;
+        }
+    }
+
+    public static class Seq
+    {
+        public static IEnumerable<A> Of<A>(params A[] vals)
+        {
+            return vals;
+        }
+
+        public static IEnumerable<A> Intersperse<A>(this IEnumerable<A> seq, A seperator)
+        {
+            var itr = seq.GetEnumerator();
+
+            if (!itr.MoveNext())
+                yield break;
+
+            yield return itr.Current;
+
+            while (itr.MoveNext())
+            {
+                yield return seperator;
+                yield return itr.Current;
+            }
+        }
+
+        /// <summary>Infinitely enumerates sequence.</summary>
+        public static IEnumerable<A> Cycle<A>(this IEnumerable<A> seq)
+        {
+            var list = new List<A>();
+
+            foreach (var item in seq)
+            {
+                list.Add(item);
+                yield return item;
+            }
+
+            while (true)
+                foreach (var item in list)
+                    yield return item;
+
+            // ReSharper disable once FunctionNeverReturns
+        }
+
+        /// <summary>Infinitely repeats item(s).</summary>
+        public static IEnumerable<A> Cycle<A>(params A[] vals)
+        {
+            return vals.Cycle();
+        }
+
+        /// <summary>Infinitely enumerates items returned from provided function.</summary>
+        public static IEnumerable<A> Forever<A>(Func<A> f)
+        {
+            while (true)
+                yield return f();
+
+            // ReSharper disable once FunctionNeverReturns
+        }
+
+        /// <summary>Performs side-effecting Action on each item in sequence.</summary>
+        public static IEnumerable<A> ForEach<A>(this IEnumerable<A> seq, Action<A> f)
+        {
+            foreach (var item in seq)
+            {
+                f(item);
+                yield return item;
+            }
+        }
+
+        /// <summary>Forces sequence to enumerate.</summary>
+        public static IEnumerable<A> Force<A>(this IEnumerable<A> seq)
+        {
+            return seq.ToArray();
+        }
+    }
 }
