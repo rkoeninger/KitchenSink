629cbc2f1508ab2adad8f50566a18a0ab87ff086
rkoeninger <rkoeninger@att.net>
2015-01-12 13:59:15 -0500

    Refactored Needs tests; Added some Needs tests

--- ZedSharp.UnitTests/NeedsTests.cs
+++ ZedSharp.UnitTests/NeedsTests.cs
@@ -7,41 +7,83 @@ namespace ZedSharp.UnitTests
     public class NeedsTests
     {
         [TestMethod]
-        public void NeedsLookup()
+        public void LookupAllExplicit()
         {
             var x = new WS();
             var y = new DbW();
             var z = new DbR();
             var w = new UI();
 
-            var deps = new Needs();
-            deps.Set<IWebService>(x);
-            deps.Set<IDatabaseCommand>(y);
-            deps.Set<IDatabaseQuery>(z);
-            deps.Set<IUserInterface>(w);
+            var needs = new Needs();
+            needs.Set<IWebService>(x);
+            needs.Set<IDatabaseCommand>(y);
+            needs.Set<IDatabaseQuery>(z);
+            needs.Set<IUserInterface>(w);
 
-            Expect.Some(z, deps.Get<IDatabaseQuery>());
-            Expect.Some(y, deps.Get<IDatabaseCommand>());
-            Expect.Some(w, deps.Get<IUserInterface>());
-            Expect.Some(x, deps.Get<IWebService>());
+            Expect.Some(z, needs.Get<IDatabaseQuery>());
+            Expect.Some(y, needs.Get<IDatabaseCommand>());
+            Expect.Some(w, needs.Get<IUserInterface>());
+            Expect.Some(x, needs.Get<IWebService>());
         }
+
+        class DbW : IDatabaseCommand { }
+        class DbR : IDatabaseQuery { }
+        class UI : IUserInterface { }
+        class WS : IWebService { }
+        public interface IDatabaseQuery { }
+        public interface IDatabaseCommand { }
+        public interface IUserInterface { }
+        public interface IWebService { }
 
         [TestMethod]
-        public void NeedsDefaultImplLookup()
+        public void DefaultImplLookup()
         {
-            var clock = new Needs().Get<IClock>().OrElseThrow(() => new Exception("No clock impl found"));
-
-            // internal protection level, so we can't refer to typeof(LiveClock)
-            Assert.IsTrue(clock.GetType().Name.Contains("LiveClock"));
+            var needs = new Needs();
+            var thing = needs.Get<IAnInterface>().OrElseThrow(() => new Exception("No impl found"));
+            Assert.IsInstanceOfType(thing, typeof(AnImplementation));
         }
+
+        [DefaultImplementation(typeof(AnImplementation))]
+        public interface IAnInterface { }
+
+        public class AnImplementation : IAnInterface { }
+
+        [TestMethod]
+        public void DefaultImplOfLookup()
+        {
+            var needs = new Needs();
+            var thing = needs.Get<ISomeInterface>().OrElseThrow(() => new Exception("No impl found"));
+            Assert.IsInstanceOfType(thing, typeof(SomeImplementation));
+        }
+
+        public interface ISomeInterface { }
+
+        [DefaultImplementationOf(typeof(ISomeInterface))]
+        public class SomeImplementation : ISomeInterface { }
+
+        [TestMethod]
+        public void MultipleDefaultImplsLookup()
+        {
+            var needs = new Needs();
+            Expect.Error(() => needs.Get<IMyInterface>());
+        }
+
+        public interface IMyInterface { }
+
+        [DefaultImplementationOf(typeof(IMyInterface))]
+        public class MyImpl1 : IMyInterface { }
+
+        [DefaultImplementationOf(typeof(IMyInterface))]
+        public class MyImpl2 : IMyInterface { }
+
+        [TestMethod]
+        public void NoImplAvailable()
+        {
+            var needs = new Needs();
+            Expect.None(needs.Get<IOtherInterface>());
+        }
+
+        public interface IOtherInterface { }
     }
 
-    class DbW : IDatabaseCommand { }
-    class DbR : IDatabaseQuery { }
-    class UI : IUserInterface { }
-    class WS : IWebService { }
-    public interface IDatabaseQuery { }
-    public interface IDatabaseCommand { }
-    public interface IUserInterface { }
-    public interface IWebService { }
 }
