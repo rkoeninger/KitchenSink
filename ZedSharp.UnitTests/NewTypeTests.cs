b50d84e517a4d304eb76bd6249c8c4192c727917
rkoeninger <rkoeninger@att.net>
2015-06-09 14:19:45 -0400

    Added NewType and tests for it

---
+++ ZedSharp.UnitTests/NewTypeTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZedSharp.UnitTests
+{
+    [TestClass]
+    public class NewTypeTests
+    {
+        public sealed class CustomerId : NewType<int> { public CustomerId(int x) : base(x) {} }
+        public sealed class ProductCode : NewType<string> { public ProductCode(string x) : base(x) {} }
+
+        [TestMethod]
+        public void NewTypeEquality()
+        {
+            var c1 = new CustomerId(453);
+            var c2 = new CustomerId(453);
+            var c3 = new CustomerId(521);
+            const int x1 = 453;
+
+            Assert.AreEqual(c1, c2);    // Same wrapped value
+            Assert.AreNotEqual(c1, c3); // Different values
+            Assert.AreNotEqual(c1, x1); // NewType<A> and A are never equal
+        }
+
+        [TestMethod]
+        public void NewTypeHashCode()
+        {
+            var c1 = new CustomerId(946);
+            const int x1 = 946;
+
+            Assert.AreEqual(x1.GetHashCode(), c1.GetHashCode());
+        }
+
+        [TestMethod]
+        public void NewTypeToString()
+        {
+            Assert.AreEqual("", new ProductCode(null).ToString());
+            Assert.AreEqual("54F23N", new ProductCode("54F23N").ToString());
+            Assert.AreEqual("123", new CustomerId(123).ToString());
+        }
+    }
+}
---
+++ ZedSharp/NewType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ZedSharp
+{
+    /// <summary>
+    /// Used to specialize another type.
+    /// </summary>
+    public abstract class NewType<A>
+    {
+        public A Value { get; private set; }
+
+        protected NewType(A value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Returns string representation of wrapped value, "" for null.
+        /// </summary>
+        public override String ToString()
+        {
+            return Value.IsNull() ? "" : Value.ToString();
+        }
+
+        /// <summary>
+        /// Gets hash code for wrapped value, 0 for null.
+        /// </summary>
+        public override int GetHashCode()
+        {
+            return Value.IsNull() ? 0 : Value.GetHashCode();
+        }
+
+        /// <summary>
+        /// Checks for equality of this NewType against another NewType.
+        /// Argument must be a NewType&lt;A&gt;.
+        /// 5 will not be equal to NewType(5).
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            return obj is NewType<A> && Equals(Value, ((NewType<A>) obj).Value);
+        }
+    }
+}
