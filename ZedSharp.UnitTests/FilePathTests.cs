2146cd4f3de2a10b977d5a697b72c476d7a1bbaa
rkoeninger <rkoeninger@att.net>
2015-07-06 12:09:38 -0400

    Added FilePath

---
+++ ZedSharp.UnitTests/FilePathTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZedSharp.UnitTests
+{
+    [TestClass]
+    public class FilePathTests
+    {
+        [TestMethod]
+        public void ConcatPaths()
+        {
+            Assert.AreEqual(
+                @"C:\Program Files (x86)\Microsoft Visual Studio 2013",
+                Drive.C / "Program Files (x86)" / "Microsoft Visual Studio 2013");
+
+            Assert.AreEqual(
+                @"\\somemachine\someshare\subpath\file.txt",
+                UNC.Host("somemachine").Share("someshare") / "subpath" / "file.txt");
+        }
+    }
+}
---
+++ ZedSharp/FilePath.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using P = System.IO.Path;
+
+namespace ZedSharp
+{
+    public static class Drive
+    {
+        public static FilePath Letter(char letter)
+        {
+            return new FilePath(letter + @":\");
+        }
+
+        public static readonly FilePath C = Letter('C');
+        public static readonly FilePath D = Letter('D');
+        public static readonly FilePath E = Letter('E');
+
+        // TODO Drive.Windows - looks up drive running OS is installed on
+        // class Folder, TODO Folder.Home, Folder.RecyclingBin, Folder.Programs...
+        // etc
+    }
+
+    public static class UNC
+    {
+        public static FilePath Path(String host, String share)
+        {
+            return Host(host).Share(share);
+        }
+
+        public static UNCHost Host(String host)
+        {
+            return new UNCHost(host);
+        }
+    }
+
+    public sealed class UNCHost : NewType<String>
+    {
+        internal UNCHost(String host) : base(host) {}
+
+        public FilePath Share(String share)
+        {
+            return new FilePath(P.Combine(Value.StartsWith(@"\\") ? Value : @"\\" + Value, share));
+        }
+
+        public static FilePath operator /(UNCHost host, FilePath end)
+        {
+            return host.Share(end.Value);
+        }
+
+        public static FilePath operator /(UNCHost host, String end)
+        {
+            return host.Share(end);
+        }
+    }
+
+    public sealed class FilePath : NewType<String>
+    {
+        internal FilePath(String path) : base(path) {}
+
+        /// <summary>
+        /// Concatenates two file paths.
+        /// </summary>
+        public static FilePath operator /(FilePath begin, FilePath end)
+        {
+            return new FilePath(P.Combine(begin.Value, end.Value));
+        }
+
+        /// <summary>
+        /// Concatenates two file paths.
+        /// </summary>
+        public static FilePath operator /(FilePath begin, String end)
+        {
+            return new FilePath(P.Combine(begin.Value, end));
+        }
+
+        /// <summary>
+        /// Concatenates two file paths.
+        /// </summary>
+        public static FilePath operator /(String begin, FilePath end)
+        {
+            return new FilePath(P.Combine(begin, end.Value));
+        }
+
+        public static implicit operator String(FilePath filePath)
+        {
+            return filePath.Value;
+        }
+    }
+}
