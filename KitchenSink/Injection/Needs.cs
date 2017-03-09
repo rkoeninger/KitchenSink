d672f3cde6e3e8faf2e469848925a5bcaa2e3cd8
Robert Koeninger <rkoeninger@att.net>
2017-03-08 21:52:24 -0500

    Moved Types into TypeExtensions

--- KitchenSink/Injection/Needs.cs
+++ KitchenSink/Injection/Needs.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using KitchenSink.Extensions;
 using static KitchenSink.Operators;
 
 namespace KitchenSink.Injection
