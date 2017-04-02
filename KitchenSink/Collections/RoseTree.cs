19308480e6dd6fcc132ca1a6fef8b1603d015a44
Robert Koeninger <rkoeninger@att.net>
2017-04-02 16:57:33 -0400

    Adding RoseTree

---
+++ KitchenSink/Collections/RoseTree.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KitchenSink.Collections
+{
+    public class RoseTree<A>
+    {
+        public RoseTree(A value)
+        {
+            Value = value;
+            Children = new List<RoseTree<A>>();
+        }
+
+        public RoseTree(A value, IEnumerable<RoseTree<A>> children)
+        {
+            Value = value;
+            Children = children.ToList();
+        }
+
+        public A Value { get; }
+
+        public IList<RoseTree<A>> Children { get; }
+    }
+}
