79c142b36461cd334539c3b5be486d44f9092167
Robert Koeninger <rkoeninger@att.net>
2017-04-19 16:51:52 -0400

    Added Concurrent.Lock

---
+++ KitchenSink/Concurrent/Lock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace KitchenSink.Concurrent
+{
+    public class Lock
+    {
+        private readonly object handle = new object();
+
+        public A Do<A>(Func<A> f)
+        {
+            lock (handle)
+            {
+                return f();
+            }
+        }
+
+        public void Do(Action f)
+        {
+            lock (handle)
+            {
+                f();
+            }
+        }
+    }
+
+    public class Lock<A>
+    {
+        private readonly Lock @lock = new Lock();
+        private A val;
+
+        public Lock()
+        {
+        }
+
+        public Lock(A val)
+        {
+            this.val = val;
+        }
+
+        public A Value
+        {
+            get { return @lock.Do(() => val); }
+            set { @lock.Do(() => { val = value; }); }
+        }
+
+        public A Do(Func<A, A> f) => @lock.Do(() => f(val));
+
+        public void Do(Action<A> f)
+        {
+            @lock.Do(() => f(val));
+        }
+
+        public A Map(Func<A, A> f) => @lock.Do(() => val = f(val));
+    }
+}
