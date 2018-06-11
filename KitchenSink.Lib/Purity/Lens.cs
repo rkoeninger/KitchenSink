0218eadc2d658b3ff33d9eb506b1dfe015864fa7
Robert Koeninger <rkoeninger@att.net>
2018-06-11 14:27:21 -0400

    Cleanup in Lens

--- KitchenSink.Lib/Purity/Lens.cs
+++ KitchenSink.Lib/Purity/Lens.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using KitchenSink.Extensions;
-using static KitchenSink.Operators;
 
 namespace KitchenSink.Purity
 {
@@ -22,35 +21,17 @@ namespace KitchenSink.Purity
         public static Lens<A, B> Of<A, B>(Expression<Func<A, B>> getExpr) =>
             new Lens<A, B>(getExpr.Compile(), Setter<A, B>(Parse(getExpr)));
 
-        private static string Parse<A, B>(Expression<Func<A, B>> getExpr)
-        {
-            if (IsNot<MemberExpression>(getExpr.Body))
-            {
-                throw new ArgumentException("Expression must be a property");
-            }
-
-            var memberExpr = (MemberExpression)getExpr.Body;
-
-            if (IsNot<PropertyInfo>(memberExpr.Member))
-            {
-                throw new ArgumentException("Expression must be a property");
-            }
-
-            return ((PropertyInfo)memberExpr.Member).Name;
-        }
+        private static string Parse<A, B>(Expression<Func<A, B>> getExpr) =>
+            ((getExpr.Body as MemberExpression)?.Member as PropertyInfo)?.Name
+            ?? throw new ArgumentException("Expression must be a property");
 
         private static Func<A, B, A> Setter<A, B>(string name)
         {
             var ctor = typeof(A)
                 .GetConstructors()
-                .SingleOrDefault(c => c.GetParameters().Length > 0);
-
-            if (ctor == null)
-            {
-                throw new InvalidOperationException(
+                .SingleOrDefault(c => c.GetParameters().Length > 0)
+                ?? throw new InvalidOperationException(
                     $"Type {typeof(A)} has more than one constructor");
-            }
-
             var properties = typeof(A).GetProperties();
             var paramz = ctor.GetParameters();
             return (record, value) =>
@@ -64,14 +45,9 @@ namespace KitchenSink.Purity
 
         private static object Get<A>(object target, IEnumerable<PropertyInfo> properties, ParameterInfo param)
         {
-            var property = properties.FirstOrDefault(x => x.Name.IsSimilar(param.Name));
-
-            if (property == null)
-            {
-                throw new InvalidOperationException(
+            var property = properties.FirstOrDefault(x => x.Name.IsSimilar(param.Name))
+                ?? throw new InvalidOperationException(
                     $"Constructor for type {typeof(A)} has parameters that do not match properties");
-            }
-
             return property.GetValue(target, null);
         }
     }
