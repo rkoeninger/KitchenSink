e249aa7ad18e6a3b8a5841f72b1ea243935e6b60
Robert Koeninger <rkoeninger@att.net>
2018-06-19 16:28:12 -0400

    Added GenericCache<A, B>

---
+++ KitchenSink.Lib/GenericCache.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace KitchenSink
+{
+    public class GenericCache<A, B>
+    {
+        private readonly ConcurrentDictionary<A, (DateTime, B)> cache;
+        private readonly TimeSpan duration;
+        private readonly Func<A, B> lookup;
+
+        public GenericCache(Func<A, B> lookup) : this(TimeSpan.MaxValue, lookup) { }
+
+        public GenericCache(TimeSpan duration, Func<A, B> lookup)
+        {
+            cache = new ConcurrentDictionary<A, (DateTime, B)>();
+            this.duration = duration;
+            this.lookup = lookup;
+        }
+
+        public B Get(A key) =>
+            cache.AddOrUpdate(
+                key,
+                k => (DateTime.UtcNow, lookup(k)),
+                (k, v) => v.Item1 < DateTime.UtcNow + duration
+                    ? (DateTime.UtcNow, lookup(k))
+                    : v).Item2;
+    }
+}
