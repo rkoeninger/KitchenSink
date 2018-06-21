eb76b30255ef1eae92e78a79f5c476cd7f49bb6b
Robert Koeninger <rkoeninger@att.net>
2018-06-21 12:08:17 -0400

    Added Auto-flush to buffer

--- KitchenSink.Lib/Buffer.cs
+++ KitchenSink.Lib/Buffer.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using KitchenSink.Concurrent;
 
 namespace KitchenSink
 {
     /// <summary>
     /// Generic buffer that passes accumulated values to given handler
-    /// when size limit is reached.
+    /// when size limit is reached and can auto-flush after a given timeout.
     /// </summary>
     /// <example>
     /// var b = Buffer.Of(1000, lines => File.AppendAllLines(path, lines));
@@ -16,21 +18,44 @@ namespace KitchenSink
     public static class Buffer
     {
         public static Buffer<A> Of<A>(long limit, Action<IReadOnlyList<A>> handler) =>
-            new Buffer<A>(limit, handler);
+            new Buffer<A>(limit, TimeSpan.Zero, handler);
+
+        public static Buffer<A> Of<A>(TimeSpan timeout, Action<IReadOnlyList<A>> handler) =>
+            new Buffer<A>(long.MaxValue, timeout, handler);
+
+        public static Buffer<A> Of<A>(long limit, TimeSpan timeout, Action<IReadOnlyList<A>> handler) =>
+            new Buffer<A>(limit, timeout, handler);
     }
 
-    public class Buffer<A>
+    public class Buffer<A> : IDisposable
     {
         private readonly long limit;
         private readonly Action<IReadOnlyList<A>> handler;
         private readonly List<A> items = new List<A>();
         private readonly Lock @lock = Lock.New();
+        private readonly CancellationTokenSource cancel;
+        private readonly Task flusher;
+        private bool running = true;
 
-        public Buffer(long limit, Action<IReadOnlyList<A>> handler)
+        public Buffer(long limit, TimeSpan timeout, Action<IReadOnlyList<A>> handler)
         {
             this.limit = limit;
             this.handler = handler;
+
+            if (timeout > TimeSpan.Zero)
+            {
+                cancel = new CancellationTokenSource();
+                flusher = Task.Run(() =>
+                {
+                    while (running)
+                    {
+                        Task.Delay(timeout, cancel.Token).ContinueWith(_ => Flush()).Wait();
+                    }
+                });
+            }
         }
+
+        public bool IsAsyncFlushRunning => running && (flusher?.IsCompleted ?? false);
 
         public void Write(A item)
         {
@@ -55,6 +80,14 @@ namespace KitchenSink
                     items.Clear();
                 }
             });
+        }
+
+        public void Dispose()
+        {
+            running = false;
+            cancel?.Cancel();
+            flusher?.Wait();
+            Flush();
         }
     }
 }
