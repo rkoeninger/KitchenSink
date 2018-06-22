bc384b418a51d9dc4df3da2f801159fb96701884
Robert Koeninger <rkoeninger@att.net>
2018-06-22 16:31:06 -0400

    Retry Subdivide WIP

--- KitchenSink.Lib/Retry.cs
+++ KitchenSink.Lib/Retry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using KitchenSink.Extensions;
 
 namespace KitchenSink
@@ -36,9 +37,38 @@ namespace KitchenSink
         public static void FixedAttempts(int count, Action f, Func<Exception, bool> p) =>
             FixedAttempts(count, f.AsFunc(), p);
 
-        public static void FixedAttemptsSubdivide<A>(int count, List<A> items, Action<A> f, Func<Exception, bool> p)
+        public static void FixedAttemptsSubdivide<A>(
+            int count,
+            List<A> items,
+            Action<IReadOnlyList<A>> f,
+            Func<Exception, bool> p)
         {
 
+        }
+
+        private static int FixedAttemptsSubdivideRecur<A>(
+            int depth,
+            IReadOnlyList<A> items,
+            Action<IReadOnlyList<A>> f,
+            Func<Exception, bool> p)
+        {
+            try
+            {
+                f(items);
+                return items.Count;
+            }
+            catch (Exception e)
+            {
+                if (!p(e))
+                {
+                    throw;
+                }
+
+                var halfLength = items.Count / 2;
+                var x = FixedAttemptsSubdivideRecur(depth - 1, items.Take(halfLength).ToList(), f, p);
+                var y = FixedAttemptsSubdivideRecur(depth - 1, items.Skip(halfLength).ToList(), f, p);
+                return x + y;
+            }
         }
     }
 }
