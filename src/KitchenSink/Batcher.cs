5682b9fdab97fd12b5fccb971bb09805b709ee05
Robert Koeninger <rkoeninger@att.net>
2020-10-23 20:32:37 -0400

    Added Sprinkler, renamed Buffer->Batcher, IO->Io

---
+++ src/KitchenSink/Batcher.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using static KitchenSink.Operators;
+
+namespace KitchenSink
+{
+    /// <summary>
+    /// Creation methods for <see cref="Batcher{A}"/>'s.
+    /// </summary>
+    public static class Batcher
+    {
+        /// <summary>
+        /// Creates a buffer which flushes when batch size is reached.
+        /// </summary>
+        public static Batcher<A> Of<A>(long limit, Action<IReadOnlyCollection<A>> handler) =>
+            new Batcher<A>(limit, TimeSpan.Zero, handler);
+
+        /// <summary>
+        /// Creates a buffer which flushes when time limit is reached.
+        /// </summary>
+        public static Batcher<A> Of<A>(TimeSpan timeout, Action<IReadOnlyCollection<A>> handler) =>
+            new Batcher<A>(long.MaxValue, timeout, handler);
+
+        /// <summary>
+        /// Creates a buffer which flushes when either batch size or time limit is reached.
+        /// </summary>
+        public static Batcher<A> Of<A>(long limit, TimeSpan timeout, Action<IReadOnlyCollection<A>> handler) =>
+            new Batcher<A>(limit, timeout, handler);
+    }
+
+    /// <summary>
+    /// Multi-threaded batcher that batches items over time and writes batches
+    /// to an arbitrary handler.
+    /// </summary>
+    /// <example>
+    /// using var b = Batcher.Of(1000, lines => File.AppendAllLines(path, lines));
+    /// foreach (var x in xs) { b.Write(x); } // buffer will occassionally flush
+    /// // some time later...
+    /// // buffer will dispose and flush remaining lines at end of scope
+    /// </example>
+    public class Batcher<A> : IDisposable
+    {
+        private readonly long limit;
+        private readonly Action<IReadOnlyCollection<A>> handler;
+        private readonly ConcurrentQueue<A> queue = new ConcurrentQueue<A>();
+        private readonly IDisposable worker;
+        private bool disposal;
+
+        internal Batcher(long limit, TimeSpan timeout, Action<IReadOnlyCollection<A>> handler)
+        {
+            if (timeout < TimeSpan.Zero)
+            {
+                throw new ArgumentException($"{nameof(timeout)} must be positive in duration");
+            }
+
+            this.limit = limit;
+            this.handler = handler;
+
+            if (timeout > TimeSpan.Zero)
+            {
+                worker = Repeat(timeout, FlushInternal);
+            }
+        }
+
+        /// <summary>
+        /// Writes an item to the buffer which will immediately be written as a batch
+        /// if the batch size has been reached or batched when criteria are met.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If Dispose has been called.</exception>
+        public void Push(A item)
+        {
+            if (disposal)
+            {
+                throw new InvalidOperationException("Already disposed or disposal in progress");
+            }
+
+            queue.Enqueue(item);
+
+            if (queue.Count >= limit)
+            {
+                FlushInternal();
+            }
+        }
+
+        /// <summary>
+        /// Writes a batch of all currently buffered items.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If Dispose has been called.</exception>
+        public void Flush()
+        {
+            if (disposal)
+            {
+                throw new InvalidOperationException("Already disposed or disposal in progress");
+            }
+
+            FlushInternal();
+        }
+
+        /// <summary>
+        /// Begins the disposal process. Buffer will flush and then be unusable.
+        /// Does nothing on subsequent calls.
+        /// </summary>
+        public void Dispose()
+        {
+            if (!disposal)
+            {
+                disposal = true;
+                worker?.Dispose();
+                FlushInternal();
+            }
+        }
+
+        private void FlushInternal()
+        {
+            lock (this)
+            {
+                var batch = new List<A>();
+
+                while (queue.TryDequeue(out var item))
+                {
+                    batch.Add(item);
+                }
+
+                if (batch.Count > 0)
+                {
+                    handler(batch);
+                }
+            }
+        }
+    }
+}
---
+++ src/KitchenSink/Sprinkler.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using static KitchenSink.Operators;
+
+namespace KitchenSink
+{
+    /// <summary>
+    /// Creation methods for <see cref="Sprinkler{A}"/>'s.
+    /// </summary>
+    public static class Sprinkler
+    {
+        /// <summary>
+        /// Creates a sprinkler which emits items continuously.
+        /// </summary>
+        public static Sprinkler<A> Of<A>(Action<A> handler) =>
+            new Sprinkler<A>(long.MaxValue, TimeSpan.Zero, handler);
+
+        /// <summary>
+        /// Creates a sprinkler which emits items with given delay between them.
+        /// </summary>
+        public static Sprinkler<A> Of<A>(TimeSpan delay, Action<A> handler) =>
+            new Sprinkler<A>(long.MaxValue, delay, handler);
+
+        /// <summary>
+        /// Creates a sprinkler which emits items with given delay between them
+        /// or emits all items continuously once queued item limit is reached.
+        /// </summary>
+        public static Sprinkler<A> Of<A>(long limit, TimeSpan delay, Action<A> handler) =>
+            new Sprinkler<A>(limit, delay, handler);
+    }
+
+    /// <summary>
+    /// Multi-threaded sprinkler that batches items over time and writes batches
+    /// to an arbitrary handler.
+    /// </summary>
+    /// <example>
+    /// using var s = Sprinker.Of(TimeSpan.FromSeconds(1), line => Console.WriteLine(line));
+    /// s.Push(File.ReadAllText("huge.txt"));
+    /// // some time later...
+    /// // sprinkler will dispose and flush remaining lines at end of scope
+    /// </example>
+    public class Sprinkler<A> : IDisposable
+    {
+        private readonly long limit;
+        private readonly Action<A> handler;
+        private readonly ConcurrentQueue<A> queue = new ConcurrentQueue<A>();
+        private readonly IDisposable worker;
+        private bool disposing;
+
+        internal Sprinkler(long limit, TimeSpan delay, Action<A> handler)
+        {
+            if (delay < TimeSpan.Zero)
+            {
+                throw new ArgumentException($"{nameof(delay)} must be positive in duration");
+            }
+
+            this.limit = limit;
+            this.handler = handler;
+            worker = Repeat(delay, Sprinkle);
+        }
+
+        private void Sprinkle()
+        {
+            if (queue.Count >= limit)
+            {
+                FlushInternal();
+            }
+            else if (queue.TryDequeue(out var item))
+            {
+                handler(item);
+            }
+        }
+
+        /// <summary>
+        /// Insert a batch of items into the sprinkler to be dispersed over time.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If Dispose has been called.</exception>
+        public void Push(IEnumerable<A> batch)
+        {
+            if (disposing)
+            {
+                throw new InvalidOperationException("Already disposed or disposal in progress");
+            }
+
+            foreach (var item in batch)
+            {
+                queue.Enqueue(item);
+            }
+
+            if (queue.Count > limit)
+            {
+                FlushInternal();
+            }
+        }
+
+        /// <summary>
+        /// Writes all currently buffered items.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If Dispose has been called.</exception>
+        public void Flush()
+        {
+            if (disposing)
+            {
+                throw new InvalidOperationException("Already disposed or disposal in progress");
+            }
+
+            FlushInternal();
+        }
+
+        /// <summary>
+        /// Begins the disposal process. Sprinkler will flush and then be unusable.
+        /// Does nothing on subsequent calls.
+        /// </summary>
+        public void Dispose()
+        {
+            disposing = true;
+            worker?.Dispose();
+            FlushInternal();
+        }
+
+        private void FlushInternal()
+        {
+            foreach (var item in queue)
+            {
+                handler(item);
+            }
+        }
+    }
+}
